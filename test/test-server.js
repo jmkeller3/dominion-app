4edd2f69f45fa3658e81c99f6f05ad71cbf00976
John Keller <jmkeller3@gmail.com>
2018-07-16 00:48:09 -0500

    test

--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,14 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
 
 describe('Web Page', function() {
-    before(function() {
-        return runServer();
-    });
-
-    after(function() {
-        return closeServer();
-    });
 
     //test strategy:
     // 1. check to see if the webpage is loading
     it('should load page upon GET', function() {
         return chai.request(app)
-            .get('/public')
+            .get('/public/index.html')
             .then(function(res) {
                 expect(res).to.have.status(200);
             })
