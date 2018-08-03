1f05e099a429d0e87f4fada8fb1f34d0b96614f8
John Keller <jmkeller3@gmail.com>
2018-08-03 16:24:20 -0500

    Added list creation get and put requests

--- models.js
+++ models.js
@@ -1,21 +1,37 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const listSchema = mongoose.Schema(
-    {
-    card1: {type: String, required: true},
-    card2: {type: String, required: true},
-    card3: {type: String, required: true},
-    card4: {type: String, required: true},
-    card5: {type: String, required: true},
-    card6: {type: String, required: true},
-    card7: {type: String, required: true},
-    card8: {type: String, required: true},
-    card9: {type: String, required: true},
-    card10: {type: String, required: true}
+const listSchema = mongoose.Schema({
+  id: uuid.v4(),
+  card1: { type: String, required: true },
+  card2: { type: String, required: true },
+  card3: { type: String, required: true },
+  card4: { type: String, required: true },
+  card5: { type: String, required: true },
+  card6: { type: String, required: true },
+  card7: { type: String, required: true },
+  card8: { type: String, required: true },
+  card9: { type: String, required: true },
+  card10: { type: String, required: true }
 });
 
-const cardList = mongoose.model('Card-List', listSchema);
+listSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    card1: this.card1,
+    card2: this.card2,
+    card3: this.card3,
+    card4: this.card4,
+    card5: this.card5,
+    card6: this.card6,
+    card7: this.card7,
+    card8: this.card8,
+    card9: this.card9,
+    card10: this.card10
+  };
+};
 
-module.exports = {cardList};
+const cardList = mongoose.model("cardlist", listSchema);
+
+module.exports = { cardList };
--- server.js
+++ server.js
@@ -1,86 +1,134 @@
-'use strict'
+"use strict";
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const uuid = require("uuid");
+const jsonParser = bodyParser.json();
 
 mongoose.Promise = global.Promise;
 
-const { PORT, DATABASE_URL } = require('./config');
-const { cardList } = require('./models');
+const { PORT, DATABASE_URL } = require("./config");
+const { cardList } = require("./models");
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //GET request to /cardlists => return all lists
 
-app.get('/cardlists', (req, res) => {
-    cardList
-        .find()
-        .then(cardlists => {
-            res.json({
-                cardlists: cardlists.map((cardlists) => cardlists.serialize())
-            });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ message: 'Internal server error'});
-        });
-})
-
-//GET request to return card lists by ID
-app.get('/cardlists/:id', (req, res) => {
-    cardList
-        .findById(req.params.id)
-        .then(cardlists => res.json(cardlists.serialize()))
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ message: 'Internal server error' });
-        });
+app.get("/cardlists", (req, res) => {
+  cardList
+    .find()
+    .then(cardlists => {
+      res.json({
+        cardlists: cardlists.map(cardlists => cardlists.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
 });
 
+//GET request to return card lists by ID
+app.get("/cardlists/:id", (req, res) => {
+  cardList
+    .findById(req.params.id)
+    .then(cardlists => res.json(cardlists.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
 
-app.use('*', function (req, res) {
-    res.status(404).json({ message: 'Not Found' });
+//POST request to add lists
+//Check to see if lists are filled.
+app.post("/cardlists", jsonParser, (req, res) => {
+  const requiredFields = [
+    "card1",
+    "card2",
+    "card3",
+    "card4",
+    "card5",
+    "card6",
+    "card7",
+    "card8",
+    "card9",
+    "card10"
+  ];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.log(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  cardList
+    .create({
+      card1: req.body.card1,
+      card2: req.body.card2,
+      card3: req.body.card3,
+      card4: req.body.card4,
+      card5: req.body.card5,
+      card6: req.body.card6,
+      card7: req.body.card7,
+      card8: req.body.card8,
+      card9: req.body.card9,
+      card10: req.body.card10
+    })
+    .then(cardlist => res.status(201).json(cardlist.serialize()))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
+app.use("*", function(req, res) {
+  res.status(404).json({ message: "Not Found" });
 });
 
 let server;
 
 function runServer(databaseURL, port = PORT) {
-
-    return new Promise((resolve, reject) => {
-        mongoose.connect(databaseURL, err => {
-            if (err) {
-                return reject(err);
-            }
-            server = app.listen(port, () => {
-                console.log(`Your app is listening on port ${port}`);
-                resolve();
-            })
-            .on('error', err => {
-                mongoose.disconnect();
-                reject(err);
-            });
-        });
-    });
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      databaseURL,
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app
+          .listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      }
+    );
+  });
 }
 
 function closeServer() {
-    return mongoose.disconnect().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing server');
-            server.close(err => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
+  });
 }
 
 if (require.main === module) {
-    runServer(DATABASE_URL).catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { app, runServer, closeServer };
