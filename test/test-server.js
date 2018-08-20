d129f70096bcccce6390186665aba7d37d023818
John Keller <jmkeller3@gmail.com>
2018-08-19 20:53:47 -0600

    discovered issue with tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,16 +1,18 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = require('chai').expect;
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = require("chai").expect;
 
-describe('Web Page', function() {
+const { app, runServer, closeServer } = require("../server");
 
-    //test strategy:
-    // 1. check to see if the webpage is loading
-    it('should load page upon GET', function() {
-        return chai.request(app)
-            .get('/index.html')
-            .then(function(res) {
-                expect(res).to.have.status(200);
-            })
-    })
-})
+describe("Web Page", function() {
+  //test strategy:
+  // 1. check to see if the webpage is loading
+  it("should load page upon GET", function() {
+    return chai
+      .request(app)
+      .get("/index.html")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
